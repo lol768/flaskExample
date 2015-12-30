dd9abee373623f631feb03ee66a7d72f3e7f6dff
lol768 <lol768@lol768.com>
2015-12-30 21:14:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import time
+app = Flask(__name__)
+
+@app.route("/") # This is the page's URL
+def homepage(): # Function to return the response for the homepage
+    #      Renders template  Template filename  Data given to template
+    return render_template("homepage.html", time=time.strftime("%c"))
+
+# Only do the webserver stuff if we're being run directly from the CLI
+if __name__ == "__main__":
+    # Allow connections from anyone, listen on port 8091
+    # Also enable pretty stack traces
+    app.run(host="0.0.0.0", port=8091, debug=True)
